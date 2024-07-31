712c04fddc1fd6c7302ec3f3e8bb98dca11b8a14
Rafaele <rafaele.sinaguglia@gmail.com>
2024-07-31 14:05:23 +0200

    searchbar

--- udemyclone/app/(dashboard)/(routes)/search/page.tsx
+++ udemyclone/app/(dashboard)/(routes)/search/page.tsx
@@ -1,5 +1,6 @@
 import { db } from "@/lib/db"
 import { Categories } from "./_components/Categories"
+import { SearchInput } from "@/components/SearchInput"
 
 
 const SearchPage =  async ()=>{
@@ -12,7 +13,7 @@ const SearchPage =  async ()=>{
     return(
         <>
         <div className="px-6 pt-6 md:hidden md:mb-0 block">
-      
+       <SearchInput/>
         </div>
         <div className="p-6 space-y-4">
           <Categories
--- udemyclone/components/navbar-routes.tsx
+++ udemyclone/components/navbar-routes.tsx
@@ -4,15 +4,24 @@ import { usePathname, useRouter } from "next/navigation"
 import { Button } from "./ui/button"
 import { LogOut } from "lucide-react"
 import Link from "next/link"
-
+import { SearchInput } from "./SearchInput"
 export const NavbarRoutes = () => {
     const pathname = usePathname();
     const router = useRouter();
 
     const isAdminPage = pathname?.startsWith("/admin");
     const isPlayerPage = pathname?.includes("/chapter");
+    const isSearchPage = pathname === "/search"
 
     return (
+        <>
+        {isSearchPage && (
+            <div className="hidden md:block">
+              <SearchInput />
+            </div>
+          )}
+        <div>
+         
         <div className="flex gap-x-2 ml-auto">
             {isAdminPage || isPlayerPage ? (
                    <Link href="/">
@@ -32,5 +41,9 @@ export const NavbarRoutes = () => {
             )}
             <UserButton />
         </div>
+
+        </div>
+
+        </>
     );
 };
